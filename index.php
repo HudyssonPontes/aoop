d0d41b9fa8f7f201948110a610a793b61996e3e5
HudyssonPontes <hudyssonpontes@protonmail.com>
2022-07-20 18:19:05 -0300

    Adição de algoritmo em função

--- index.php
+++ index.php
@@ -4,21 +4,25 @@
  </head>
  <body>
  <?php 
-    echo "<b>O email buscado é:</b> ";
-    $a=0;
-    $b=1; 
-    $string = strtolower("AcoDQeYjrSlByFtyzQhvjoCNVpsaLeQPtGUjQHVzbgabJuAiMHDxwfkNvCwIGmZCTInlSiKvRKxAGzJsgmPeUBAReDzmLzqgJgrXobzfoWiwvRPdFgJzIkSoJscWtVdEbuIRYhXOdHkayBdFIMHSyzIJtmGMhJTiFBaDIzrngCngdSnngUHFWpQpCwElHxunYWsiXKvFOkntcAHiXopTgIKkeovoLrBlPTtMfqTTAgnejUPgKeBsolCtAAjNtKBjf");
-    $arrayChar = str_split($string);
-    for($count=1 ; $count<=10 ; $count++){
-        $atual = $b + $a;
-        echo $arrayChar[$atual];
-        if($count==5){
-            echo ".";
-        }        
-        $a = $b;
-        $b = $atual;
-    } 
+ function email($string){
+     echo "<b>O email buscado é:</b> ";
+     $string = strtolower($string);
+     $a=0;
+     $b=1; 
+     $arrayChar = str_split($string);
+     for($count=1 ; $count<=10 ; $count++){
+         $atual = $b + $a;
+         echo $arrayChar[$atual];
+         if($count==5){
+             echo ".";
+         }        
+         $a = $b;
+         $b = $atual;
+     } 
     echo "@aoop.com.br";
+ }
+ $string = "AcoDQeYjrSlByFtyzQhvjoCNVpsaLeQPtGUjQHVzbgabJuAiMHDxwfkNvCwIGmZCTInlSiKvRKxAGzJsgmPeUBAReDzmLzqgJgrXobzfoWiwvRPdFgJzIkSoJscWtVdEbuIRYhXOdHkayBdFIMHSyzIJtmGMhJTiFBaDIzrngCngdSnngUHFWpQpCwElHxunYWsiXKvFOkntcAHiXopTgIKkeovoLrBlPTtMfqTTAgnejUPgKeBsolCtAAjNtKBjf";
+ email($string);
  ?>
  </body>
 </html>
